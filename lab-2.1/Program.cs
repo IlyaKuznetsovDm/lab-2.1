ef873047a7e1b8d24128c527e2285a9244632dfd
Dmitr <Dmitr@DESKTOP-H2P4N61>
2021-10-25 02:09:09 +0200

    Добавьте файлы проекта.

---
+++ lab-2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+ 
+                //2.1 средний уровень - 8 вариант
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                if (n % 3 != 0 && n % 7 == 0 && n % 10 == 0)
+                    Console.WriteLine("Yes");
+                else Console.WriteLine("No");       
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введите корректно переменную");
+            }
+        }
+    }
+}
